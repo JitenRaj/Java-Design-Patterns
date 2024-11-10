114a40ded270bec3abccde406c6db30c1530d1e1
JitenRaj <ji10raj@gmail.com>
2024-11-10 21:43:56 +0530

    Singleton design pattern

--- CreationalDesignPatterns/src/SingletonPattern/Singleton.java
+++ CreationalDesignPatterns/src/SingletonPattern/Singleton.java
@@ -1,4 +1,20 @@
 package SingletonPattern;
 
-public class Singleton {
+class Singleton {
+
+    public static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public static void doSomething() {
+        System.out.println("Something is done");
+    }
 }
--- CreationalDesignPatterns/src/SingletonPattern/Solution.java
+++ CreationalDesignPatterns/src/SingletonPattern/Solution.java
@@ -1,4 +1,8 @@
 package SingletonPattern;
 
 public class Solution {
+    public static void main(String[] args) {
+        Singleton.getInstance();
+        Singleton.doSomething();
+    }
 }
